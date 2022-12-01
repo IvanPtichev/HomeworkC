5948758f8c60920d1e4096edeb26c1e584160658
Даниил Попереков <ivanptichev@gmail.com>
2022-12-01 16:43:43 +0400

    first task solved

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+/*
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("Наибольшее число "+ numberA);
+}
+else
+{
+    Console.WriteLine("Наибольшее число "+ numberB);
+}
+*/
+
+// Задача2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+
