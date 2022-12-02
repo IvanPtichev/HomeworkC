2f3ad575aed2e15915b53de18bf0e8dda796dce9
Даниил Попереков <ivanptichev@gmail.com>
2022-12-02 15:14:44 +0400

    Completed work

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -55,3 +55,36 @@ else
     }
 }
 */
+
+// Задача3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+/*
+Console.WriteLine("Введите число, за исключением нуля");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else{
+    Console.WriteLine("Число нечётное");
+}
+*/
+
+// Задача4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.WriteLine("Введите положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+if(num>0)
+{while(current <= num)
+    {
+    Console.WriteLine(current + " ");
+    current +=2;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+*/
