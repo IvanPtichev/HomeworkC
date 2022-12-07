d7a6beb1605e3d08199aa912a5f5aa8609aea33c
Даниил Попереков <ivanptichev@gmail.com>
2022-12-07 17:24:03 +0400

    task 1 solved

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.WriteLine("enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 0) num = num * -1;
+
+int coupNum = 0;
+int temp = num;
+
+while(temp > 0)
+{
+    coupNum = coupNum * 10 + temp % 10;
+    temp = temp / 10;
+}
+if(num > 0)
+{
+    if(num == coupNum)
+    {
+    Console.WriteLine("palindrome");
+    }
+    else
+    {
+    Console.WriteLine("not a palindrome");
+    }
+}
+*/
